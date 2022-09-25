7cf31bbae24392a2bae4ccc7d133b43ce131279b
etorger <etorger@tktk.ee>
2022-09-25 12:40:28 +0300

    add week4 code

---
+++ Week4Loops/Program.cs
@@ -0,0 +1,13 @@
+﻿//for-loop
+//dry-principle - do not repeat yourself
+
+for(int i = 0; i < 5; i++) //i++ = i+1
+{
+    Console.WriteLine("We dont need no education");
+}
+
+
+Console.WriteLine(i); 
+Console.WriteLine("have a nice day");
+
+
---
+++ WhileLoops/Program.cs
@@ -0,0 +1,21 @@
+﻿Random rnd = new Random();
+int cpuRandom;
+bool loopactive = true; 
+while (loopactive)
+{
+   cpuRandom = rnd.Next(1,4);
+    Console.WriteLine($"cpu has generated {cpuRandom}");
+    Console.WriteLine("Make a guess: Enter a number 1-3");
+    int userNumber = Int32.Parse(Console.ReadLine());
+    if(userNumber == cpuRandom)
+    {
+        Console.WriteLine("Congratulation, you won!");
+        loopactive = false; 
+    }else
+    {
+        Console.WriteLine("Oops, try again.");
+    }
+
+}
+
+Console.WriteLine("Have a nice day");
